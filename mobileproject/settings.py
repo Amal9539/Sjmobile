b082f8fc66ff2792127408b0c3accb6631e237d1
Vipinraj9526 <vipinrajuk3@gmail.com>
2025-11-01 13:44:43 +0530

    sql lite integration

--- mobileproject/settings.py
+++ mobileproject/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'mobileproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mobile',
-        'USER':'root',
-        'HOST':'localhost',
-        'PASSWORD':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
